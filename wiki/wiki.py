6ad62b2f7ba6f55e3226dc00e1be33dea9e44a25
jay3ss <mentoce@gmail.com>
2025-01-02 13:57:13 -0800

    Update imports, add function to fetch occupations, update decorator

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -1,9 +1,9 @@
 import requests
 
 from config import config
-from wiki.queries import family_tree_query
-from wiki.schemas import Relationship
-from wiki.utils import flatten_to_model
+from wiki.queries import family_tree_query, occupations_query
+from wiki.schemas import Occupation, Relationship
+from wiki.utils import flatten_to_schema
 
 session = requests.Session()
 session.headers.update(
@@ -29,10 +29,23 @@ def search_wiki(query: str) -> dict:
     return results["query"]["search"]
 
 
-@flatten_to_model(Relationship)
+@flatten_to_schema(Relationship)
 def fetch_relatives(entity_id: str) -> list:
     params = {
         "query": family_tree_query.format(entity_id=entity_id),
+        "format": "json",
+    }
+    response = session.get(config.wikidata_api_url, params=params)
+
+    response.raise_for_status()
+
+    return response.json()["results"]["bindings"]
+
+
+@flatten_to_schema(Occupation)
+def fetch_occupations(entity_id: str) -> list:
+    params = {
+        "query": occupations_query.format(entity_id=entity_id),
         "format": "json",
     }
     response = session.get(config.wikidata_api_url, params=params)
