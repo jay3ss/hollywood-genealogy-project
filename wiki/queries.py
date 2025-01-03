7336c2cb7deed45a4829e8442b3056668c2ae148
jay3ss <mentoce@gmail.com>
2025-01-02 16:59:48 -0800

    Add query to get personal (biographical) information on someone

--- wiki/queries.py
+++ wiki/queries.py
@@ -48,3 +48,60 @@ WHERE {{
   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE] en". }}
 }}
 """
+
+
+personal_information_query = """
+SELECT
+  ?birth_date
+  ?death_date
+  ?gender
+  ?birth_place
+  ?biography
+WHERE {{
+  # Specify the person by their Wikidata entity ID
+  OPTIONAL {{ wd:{entity_id} wdt:P569 ?birth_date_entity. }}  # Birth date (P569)
+  OPTIONAL {{ wd:{entity_id} wdt:P570 ?death_date_entity. }}  # Death date (P570)
+  OPTIONAL {{ wd:{entity_id} wdt:P21 ?gender_entity. }}  # Gender (P21)
+  OPTIONAL {{ wd:{entity_id} wdt:P19 ?birth_place_entity. }}  # Hometown / place of birth (P19)
+  OPTIONAL {{ wd:{entity_id} schema:description ?biography. FILTER(LANG(?biography) = "en") }}  # Biography in English only
+
+  OPTIONAL {{
+    ?gender_entity rdfs:label ?gender.
+    FILTER(LANG(?gender) = "en")
+  }}
+
+  OPTIONAL {{
+    ?birth_place_entity rdfs:label ?birth_place.
+    FILTER(LANG(?birth_place) = "en")
+  }}
+
+  # Formatting birth and death dates as 'YYYY-MM-DD'
+  BIND(
+    IF(
+      BOUND(?birth_date_entity),
+      CONCAT(
+        STR(YEAR(?birth_date_entity)),
+        "-",
+        STR(MONTH(?birth_date_entity)),
+        "-",
+        STR(DAY(?birth_date_entity))
+      ),
+      ?birth_date_entity
+    ) AS ?birth_date
+  )
+
+  BIND(
+    IF(
+      BOUND(?death_date_entity),
+      CONCAT(
+        STR(YEAR(?death_date_entity)),
+        "-",
+        STR(MONTH(?death_date_entity)),
+        "-",
+        STR(DAY(?death_date_entity))
+      ),
+      ?death_date_entity
+    ) AS ?death_date
+  )
+}}
+"""
