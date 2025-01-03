5b1199d3254a412a4e93089d2ae3276c7d4877e1
jay3ss <mentoce@gmail.com>
2025-01-02 17:01:05 -0800

    Refactor to include higher-level function to query wikidata and then use it in other querying/fetching functions, add function to get personal info on a person

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -1,8 +1,12 @@
 import requests
 
 from config import config
-from wiki.queries import family_tree_query, occupations_query
-from wiki.schemas import Occupation, Relationship
+from wiki.queries import (
+    family_tree_query,
+    occupations_query,
+    personal_information_query,
+)
+from wiki.schemas import Occupation, PersonalInfo, Relationship
 from wiki.utils import flatten_to_schema
 
 session = requests.Session()
@@ -11,6 +15,18 @@ session.headers.update(
         "User-Agent": config.wikipedia_user_agent,
     }
 )
+
+
+def fetch_from_wikidata(query: str) -> list:
+    params = {
+        "query": query,
+        "format": "json",
+    }
+    response = session.get(config.wikidata_api_url, params=params)
+
+    response.raise_for_status()
+
+    return response.json()["results"]["bindings"]
 
 
 def search_wiki(query: str) -> dict:
@@ -31,25 +47,14 @@ def search_wiki(query: str) -> dict:
 
 @flatten_to_schema(Relationship)
 def fetch_relatives(entity_id: str) -> list:
-    params = {
-        "query": family_tree_query.format(entity_id=entity_id),
-        "format": "json",
-    }
-    response = session.get(config.wikidata_api_url, params=params)
-
-    response.raise_for_status()
-
-    return response.json()["results"]["bindings"]
+    return fetch_from_wikidata(family_tree_query.format(entity_id=entity_id))
 
 
 @flatten_to_schema(Occupation)
 def fetch_occupations(entity_id: str) -> list:
-    params = {
-        "query": occupations_query.format(entity_id=entity_id),
-        "format": "json",
-    }
-    response = session.get(config.wikidata_api_url, params=params)
+    return fetch_from_wikidata(occupations_query.format(entity_id=entity_id))
 
-    response.raise_for_status()
 
-    return response.json()["results"]["bindings"]
+@flatten_to_schema(PersonalInfo)
+def fetch_personal_info(entity_id: str) -> list:
+    return fetch_from_wikidata(personal_information_query.format(entity_id=entity_id))
